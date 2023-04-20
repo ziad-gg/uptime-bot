5260676822e16eaa0eb5e521850844137e0baa47
Glitch (glitch-hello-node) <none>
2023-04-20 00:16:52 +0000

    🎅🙈 Checkpoint ./commands/general/urls.js:41556106/1934

--- commands/general/urls.js
+++ commands/general/urls.js
@@ -6,13 +6,24 @@ module.exports = new Command()
   .setDescription("Get all urls by user")
   .setExecution(Execute);
 
+var Counter = 0
+
 async function Execute() {
   const { message, args } = this;
   const uptime = message.data.get("uptimerApp");
   const KEY = message.author.id;
   const DATA = uptime.get({ KEY });
+  Counter = 0;
   const embed = new EmbedBuilder()
-    .setTitle(`مجموعة الروابط المضافة (${DATA?.urls.length || 0})`)
-    .setDescription(DATA?.map(e => ).urls.join("\n") || " ");
+  .setTitle(`مجموعة الروابط المضافة (${DATA?.urls.length || 0})`)
+  .setDescription(DATA?.urls.map(getDomainLink).join("\n") || " ");
   message.reply({ embeds: [embed] });
-}
+};
+
+function getDomainLink(url) {
+  let domain = url.replace(/^https?:\/\//, '');
+  domain = domain.split('/')[0];
+  domain = domain.split('.').slice(-2).join('.');
+  ++Counter;
+  return `[**#${Counter}** ${domain}](${url})`;
+};
